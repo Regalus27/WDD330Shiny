e8db19cbd3c12abfac3061eca0bb3e4ffa4f1a80
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2024-02-10 22:56:12 -0700

    API test successful

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "src/",
-    build: {
-        outDir: "../dist",
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "src/index.html")
-            },
-        },  
+  root: "src/",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
     },
+  },
 });
